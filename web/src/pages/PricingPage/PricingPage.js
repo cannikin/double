e5cb0f160008093ba8daa23276a0c8392e15bcd6
Rob Cameron <rob.cameron@fastmail.com>
2021-09-20 16:00:11 -0700

    Add login/logout button secure page

--- web/src/pages/PricingPage/PricingPage.js
+++ web/src/pages/PricingPage/PricingPage.js
@@ -1,7 +1,19 @@
 import PricingCell from 'src/components/PricingCell'
+import { useAuth } from '@redwoodjs/auth'
 
 const PricingPage = () => {
-  return <PricingCell />
+  const { isAuthenticated, logIn, logOut } = useAuth()
+
+  return (
+    <>
+      <header>
+        <button type="button" onClick={isAuthenticated ? logOut : logIn}>
+          {isAuthenticated ? 'Log Out' : 'Log In'}
+        </button>
+      </header>
+      <PricingCell />
+    </>
+  )
 }
 
 export default PricingPage
